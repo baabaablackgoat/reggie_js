cb0ac79bfab162012344668615185b15e482bbe2
TrueMGF <nikithekid@hotmail.de>
2018-06-20 23:52:13 +0200

    Music support WIP Commit

---
+++ cmds/play.js
@@ -0,0 +1,13 @@
+const main = function(bot,globals,msg,args){
+    
+};
+module.exports = {
+    "aliases":['play'],
+    "auth":false,
+    "help": {
+        "short":"foobar",
+        "long":"foobar",
+        "args": false
+    },
+    "fn": main
+};
--- forin.js
+++
@@ -1,4 +0,0 @@
-let somestring = "test wi\th es\"cap\'e'd chars";
-for (let i in somestring){
-    console.log(i,somestring[i]);
-}
